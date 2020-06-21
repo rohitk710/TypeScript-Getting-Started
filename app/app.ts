fd1ace12ee0ff2cae0202ca336f93cab38a0e68f
Rohit <rohit.pandey@uipath.com>
2020-06-21 22:17:00 +0530

    module 2

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,48 @@
-function startGame(){
-    var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome';
+function startGame() {
+  let playerName: string | undefined = getInputValue("playername");
+  logPlayer(playerName);
+
+  postScore(100, playerName);
+  postScore(-5, playerName);
 }
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+function logPlayer(name: string = "Player"): void {
+  arrowLogMessage(`New game starting for: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementId)
+  );
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName?: string): void {
+  let logger: (value: string) => void;
+
+  if(score < 0){
+      logger = logError;
+  }
+  else {
+      logger = arrowLogMessage;
+  }
+
+  const scoreElement: HTMLElement | null = document.getElementById(
+    "posted-scores"
+  );
+  scoreElement!.innerText = `${score} - ${playerName}`;
+
+  logger(`Score: ${score}`);
+}
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
+
+function logError(message: string): void {
+  console.error(message);
+}
+
+const arrowLogMessage = (message: string): void => console.log(message);
